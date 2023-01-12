c8b2fa2b672e675657deaa34e1291b575f17fb24
Tsimpris Giannis <johntsimpris@gmail.com>
2023-01-12 14:03:55 +0200

    Add a comment Task One

--- Session-04/Session-04/Session-04/Program.cs
+++ Session-04/Session-04/Session-04/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Session_04;
 Console.WriteLine("Hello, World!");
-
+//Task One
 Taskone helloName = new Taskone();
 
 Console.WriteLine(helloName.ErvthmaEna());
