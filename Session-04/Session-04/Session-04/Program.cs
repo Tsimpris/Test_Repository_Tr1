29de1912838f231e11160e3e2ed19b33550fb3e2
Tsimpris Giannis <122268373+Tsimpris@users.noreply.github.com>
2023-01-12 14:33:43 +0200

    Delete Session-04/Session-04 directory

--- Session-04/Session-04/Session-04/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Session_04;
-Console.WriteLine("Hello, World!");
-//Task One
-Taskone helloName = new Taskone();
-
-Console.WriteLine(helloName.ErvthmaEna());
-
-Console.ReadLine();
