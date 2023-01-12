a11a162e45e44c39b8cd898d9f4fbf3d0ff4c83d
Tsimpris Giannis <johntsimpris@gmail.com>
2023-01-12 13:59:54 +0200

    Task One Session 04 Test Push

---
+++ Session-04/Session-04/Session-04/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Session_04;
+Console.WriteLine("Hello, World!");
+
+Taskone helloName = new Taskone();
+
+Console.WriteLine(helloName.ErvthmaEna());
+
+Console.ReadLine();
